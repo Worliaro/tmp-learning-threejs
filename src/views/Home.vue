a388a3b90798a29ec8e2fd637f50ca34c67f2aab
Jin-Yanhong <820877998@qq.com>
2023-03-25 11:24:02 +0800

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		{{ doubleValue }}
+
+		<button @click="doDecrement"></button>
+	</div>
+</template>
+
+<script lang="ts" setup>
+import { computed, ref } from 'vue';
+import { useCounterStore } from '../store';
+
+// access the `store` variable anywhere in the component ✨
+const store = useCounterStore();
+
+// setTimeout(() => {
+store.increment();
+// }, 1000);
+
+function doDecrement() {
+	store.decrement();
+}
+
+// ✅ this one will be reactive
+// 💡 but you could also just use `store.doubleCount` directly
+const doubleValue = computed(() => store.doubleCount);
+</script>
+
+<style lang="less"></style>
