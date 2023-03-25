87cf18f9a103cff9c37dff0c9b345e58b78cff9a
Jin-Yanhong <820877998@qq.com>
2023-03-25 16:35:12 +0800

    add request util

---
+++ src/store/modules/user.ts
@@ -0,0 +1,41 @@
+import { clearStorage, getStorage, setStorage } from '@/utils/index';
+import { defineStore } from 'pinia';
+
+interface loginFormType {
+	userName: string;
+	passWord: string;
+}
+
+export const useUserStore = defineStore({
+	id: 'user',
+	state: () => ({
+		token: getStorage('token'),
+	}),
+	getters: {
+		getToken: state => {
+			return state.token;
+		},
+	},
+	actions: {
+		async handleLogin(loginArgs: loginFormType) {
+			try {
+				const accessToken = '';
+				setStorage('token', accessToken);
+				this.$patch({
+					token: accessToken,
+				});
+			} catch (error: any) {
+				this.$patch({
+					token: '',
+				});
+				clearStorage();
+			}
+		},
+		async handleLogout() {
+			this.$patch({
+				token: '',
+			});
+			clearStorage();
+		},
+	},
+});
