a326e444019edd07d9e09326b27e4d4c6d101170
Jin-Yanhong <820877998@qq.com>
2023-03-25 14:07:36 +0800

    update config

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import ElementPlus from 'element-plus';
 import router from './router/index';
-import 'element-plus/dist/index.css';
-import './style.css';
+
+import 'normalize.css'; // css reset
+import 'element-plus/dist/index.css'; // ElementPlus style
+
 import App from './App.vue';
 
 const app = createApp(App);
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,12 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHashHistory as Router } from 'vue-router';
 import Home from '../views/Home.vue';
 
 const routes = [{ path: '/', component: Home }];
 
 const router = createRouter({
-	// 4. Provide the history implementation to use. We are using the hash history for simplicity here.
-	history: createWebHashHistory(),
-	routes, // short for `routes: routes`
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: Router(),
+    routes,
 });
 
 export default router;
